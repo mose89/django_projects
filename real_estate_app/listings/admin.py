41e6e51067906c5b26d0a6abb6d37cb2a777ab29
mose89 <mose89@gmail.com>
2020-05-24 08:52:41 +0200

    Edit design in Admin panel.

--- real_estate_app/listings/admin.py
+++ real_estate_app/listings/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Listing
 # Register your models here.
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+	list_display_links = ('id', 'title')
+	list_filter = ('realtor',)
+	list_editable = ('is_published',)
+	search_fields = ('title', 'description', 'address', 'city', 'state', 'zip_code', 'price',)
+admin.site.register(Listing, ListingAdmin)

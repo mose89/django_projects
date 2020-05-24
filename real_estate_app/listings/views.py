abaaa6ea7757d8de85aa7e7c5fbe2f6ba16b277a
mose89 <mose89@gmail.com>
2020-05-25 01:49:11 +0200

    Fix misspellings on listings, listing and index page.

--- real_estate_app/listings/views.py
+++ real_estate_app/listings/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from datetime import datetime as dt
 # Create your views here.
 def index(request):
 	listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -14,7 +14,14 @@ def index(request):
 	return render(request, 'listings/listings.html', context=context)
 
 def listing(request, listing_id):
-	return render(request, 'listings/listing.html')
+	listing = get_object_or_404(Listing, pk=listing_id)
+	realtor = listing.realtor
+	context = {
+	'listing': listing,
+	'realtor': realtor,
+	'dt': dt
+	}
+	return render(request, 'listings/listing.html', context)
 
 def search(request):
 	return render(request, 'listings/search.html')

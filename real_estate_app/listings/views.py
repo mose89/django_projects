3e770e61d79a5759b66bd46adb9a59818307c453
mose89 <mose89@gmail.com>
2020-05-24 08:19:52 +0200

    Set all template subfolders inside a new templates folder in the root. Adjust the views to render the templates with the new structure

--- real_estate_app/listings/views.py
+++ real_estate_app/listings/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'listings.html')
+	return render(request, 'listings/listings.html')
 
 def listing(request):
-	return render(request, 'listing.html')
+	return render(request, 'listings/listing.html')
 
 def search(request):
-	return render(request, 'search.html')
+	return render(request, 'listings/search.html')
--- real_estate_app/pages/views.py
+++ real_estate_app/pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'pages/index.html')
 
 def about(request):
-	return render(request, 'about.html')
+	return render(request, 'pages/about.html')

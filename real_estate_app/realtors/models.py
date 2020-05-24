36a718977e34730e852e2ce627eb658c0e0c18e6
mose89 <mose89@gmail.com>
2020-05-24 06:36:32 +0200

    create realtors and listings migrations. Add str function to models. Create media folders.

--- real_estate_app/realtors/models.py
+++ real_estate_app/realtors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from datetime import datetime as dt
 # Create your models here.
+class Realtor(models.Model):
+	name = models.CharField(max_length = 150)
+	photo = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+	description = models.TextField()
+	email = models.CharField(max_length = 100)
+	phone = models.CharField(max_length = 50)
+	is_mvp = models.BooleanField(default = False)
+	hire_date = models.DateTimeField(default=dt.now, blank=True)
+
+	def __str__(self):
+		return self.name

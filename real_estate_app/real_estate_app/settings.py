c7dd82d90f9730be95f7bf3e67b2bc5187c40e0c
mose89 <mose89@gmail.com>
2020-05-26 01:53:32 +0200

    Att alerts partial to templates. Enable messages in settings. Create login and register layout. enable user registration, login and logout in views. Validate POST request for user creation and login in views.

--- real_estate_app/real_estate_app/settings.py
+++ real_estate_app/real_estate_app/settings.py
@@ -132,3 +132,9 @@ os.path.join(BASE_DIR, 'real_estate_app/static')
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}

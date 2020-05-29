996734dab17913e9f939e437f9bfde55b1c31ecf
mose89 <mose89@gmail.com>
2020-05-29 04:26:28 +0200

    Create contacts app
    Added a contacts model in the contacts.models folder

---
+++ real_estate_app/contacts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from datetime import datetime as dt
+
+# Create your models here.
+
+class contacts(models.Model):
+	"""docstring for contacts"""
+	listing = models.CharField(max_length = 200)
+	listing_id = models.IntegerField()
+	name = models.CharField(max_length = 100)
+	email = models.CharField(max_length = 100)
+	phone = models.CharField(max_length = 100)
+	message = models.TextField(blank=True, max_length = 750)
+	contact_date = models.DateTimeField(blank=true, default = dt.now)
+	user_id = models.IntegerField(blank=True)
+	
+	def __str__(self):
+		return self.name
